9eb16e0bd8a3ec423c05ecb569da1f1781d6de21
mahmoud elshenawy <yugigx6@gmail.com>
2024-05-12 14:29:25 +0300

    feat: Add users module and its crud

---
+++ src/components/Users/UsersModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UsersSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'User Name Is Required'],
+    },
+    email: {
+      type: String,
+      index: { unique: true },
+      required: [true, 'User Email Is Required'],
+    },
+    password: {
+      type: String,
+      required: [true, 'User Password Is Required'],
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  },
+);
+
+module.exports.Users = mongoose.model('User', UsersSchema);
